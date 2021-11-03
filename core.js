02b8dcbbd126199ebaf9c238a1feb50cf698047a
cweijan <cweijan@163.com>
2021-11-03 12:42:15 +0800

    优化ip获取.

--- core.js
+++ core.js
@@ -5,9 +5,13 @@ const configPath = __dirname + "/config.json";
 
 
 function getIP() {
-    return axios.get("https://jsonip.com/").then(res=>{
-        return res.data.ip;
-    }).catch(err=>null)
+    return axios
+        // .get("https://jsonip.com/").then(res => res.data.ip) // 270ms
+        // .get("http://httpbin.org/ip").then(res => res.data.origin) // 260ms
+        // .get("http://pv.sohu.com/cityjson").then(res => res.data.ip) // 60ms, 返回的是一个变量, 需要处理
+        // .get("http://checkip.amazonaws.com/").then(res => res.data.trim()) // 240ms
+        .get("http://ip.threep.top/").then(res => res.data.trim()) // 100ms
+        .catch(err => null)
 }
 
 function getCofnig() {
--- test/testGetIP.js
+++ test/testGetIP.js
@@ -1,9 +1,7 @@
 const { getIP } = require("../core");
 
 async function testGetIP(){
-
     const ip=await getIP()
     console.log(ip)
-
 }
 testGetIP()
