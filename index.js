52727b50ad84899775d8e3529f551c7fdd923bf4
cweijan <cweijan@163.com>
2021-11-12 14:14:13 +0800

    增加重复运行监测.

--- index.js
+++ index.js
@@ -4,30 +4,41 @@ const express = require('express')
 const fs = require('fs')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { getCofnig,requestDispath,startCron } = require('./core');
+const { getCofnig, requestDispath, startCron } = require('./core');
 const app = express()
+const tcpPortUsed = require('tcp-port-used');
 const open = require('open');
 
-const configPath = __dirname + "/config.json";
 const port = 7231;
-app.use(cors()).use(bodyParser.urlencoded({ extended: true }))
-    .use(bodyParser.json())
-    .use(express.static(__dirname + '/dist'))
-    .get("/config", async (req, res) => {
-        res.json(await getCofnig());
-    })
-    .post("/config", async (req, res) => {
-        fs.writeFileSync(configPath, JSON.stringify(req.body, null, 2))
-        res.json(await getCofnig())
-    })
-    .get("/startDDNS", async (req, res) => {
-        const result = await requestDispath()
-        console.log(result)
-        res.json({})
-    })
-    .listen(port, () => {
-        startCron();
-        const target = `http://127.0.0.1:${port}`;
-        console.log(`启动DDNS程序成功! 访问地址为: ${target}!`);
+const target = `http://127.0.0.1:${port}`;
+
+tcpPortUsed.check(port, '127.0.0.1').then((inUse) => {
+    if (inUse) {
+        console.log('DDNS程序已经启动..')
         open(target)
-    });
+        return;
+    }
+    const configPath = __dirname + "/config.json";
+    app.use(cors()).use(bodyParser.urlencoded({ extended: true }))
+        .use(bodyParser.json())
+        .use(express.static(__dirname + '/dist'))
+        .get("/config", async (req, res) => {
+            res.json(await getCofnig());
+        })
+        .post("/config", async (req, res) => {
+            fs.writeFileSync(configPath, JSON.stringify(req.body, null, 2))
+            res.json(await getCofnig())
+        })
+        .get("/startDDNS", async (req, res) => {
+            const result = await requestDispath()
+            console.log(result)
+            res.json({})
+        })
+        .listen(port, () => {
+            startCron();
+            console.log(`启动DDNS程序成功! 访问地址为: ${target}!`);
+            open(target)
+        });
+
+});
+
